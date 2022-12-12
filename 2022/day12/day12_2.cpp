734090271ed3017482d8b4664d97271818d3a1c6
MarcoMeinardi <marcomeinardi01@gmail.com>
2022-12-12 10:13:26 +0100

    day12

---
+++ 2022/day12/day12_2.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector <string> grid;
+
+const int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
+
+int bfs(pair <int, int> finish) {
+	vector <vector <bool>> vis(n, vector <bool> (m, false));
+
+	queue <array <int, 3>> todo;
+
+	todo.push({finish.first, finish.second, 0});
+
+	while (!todo.empty()) {
+		array <int, 3>& front = todo.front();
+		int py = front[0], px = front[1], w = front[2];
+		todo.pop();
+		if (grid[py][px] == 'a') {
+			return w;
+		}
+		if (vis[py][px]) continue;
+		vis[py][px] = true;
+
+		for (int i = 0; i < 4; i++) {
+			int ny = py + dy[i], nx = px + dx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m ||
+				vis[ny][nx] || grid[py][px] - grid[ny][nx] > 1) continue;
+
+			todo.push({ny, nx, w + 1});
+		}
+	}
+	assert(false);
+}
+
+int main () {
+	string line;
+	cin >> line;
+	m = (int)line.size();
+	do {
+		grid.push_back(line);
+	} while (cin >> line);
+
+	n = (int)grid.size();
+
+	pair <int, int> start, finish;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (grid[i][j] == 'S') {
+				start = make_pair(i, j);
+				grid[i][j] = 'a';
+			} else if (grid[i][j] == 'E') {
+				finish = make_pair(i, j);
+				grid[i][j] = 'z';
+			}	
+		}
+	}
+
+	cout << bfs(finish) << endl;
+}
+
