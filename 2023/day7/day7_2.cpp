a691426d1b743bf5046c3d311ce0d1b0b0b13f3d
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-12-07 13:05:59 +0100

    day7

---
+++ 2023/day7/day7_2.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define HAND_SIZE 5
+#define JOLLY 1
+
+char values[0x80];
+
+__attribute__((constructor)) void set_values()
+{
+	for (int i = '2'; i <= '9'; i++) {
+		values[i] = i - '0';
+	}
+	values['T'] = 10;
+	values['J'] = JOLLY;
+	values['Q'] = 12;
+	values['K'] = 13;
+	values['A'] = 14;
+}
+
+struct hand_t {
+	uint64_t bid;
+	char cards[8];
+	int score;
+
+	hand_t (string cards_str, uint64_t bid) : bid(bid)
+	{
+		for (int i = 0; i < HAND_SIZE; i++) {
+			cards[i] = values[(size_t)cards_str[i]];
+		}
+
+		get_score();
+	}
+
+	void get_score()
+	{
+		int freq[16] = {0};
+		for (int i = 0; i < HAND_SIZE; i++) {
+			freq[(size_t)cards[i]]++;
+		}
+
+		if (freq[JOLLY] >= 4) {
+			score = 6;
+			return;
+		}
+
+		bool has_three = false, has_two = false;
+		for (int i = 2; i < 15; i++) {
+			if (freq[i] == 5) {
+				score = 6;
+				return;
+			} else if (freq[i] == 4) {
+				score = 5 + (freq[JOLLY] == 1);
+				return;
+			} else if (freq[i] == 3) {
+				has_three = true;
+			} else if (freq[i] == 2) {
+				if (has_two) {
+					score = freq[JOLLY] == 1 ? 4 : 2;
+					return;
+				} else {
+					has_two = true;
+				}
+			}
+		}
+
+		if (has_three && has_two) {
+			score = 4;
+		} else if (has_three) {
+			score = freq[JOLLY] == 2 ? 6 : freq[JOLLY] == 1 ? 5 : 3;
+		} else if (has_two) {
+			score = freq[JOLLY] == 3 ? 6 : freq[JOLLY] == 2 ? 5 : freq[JOLLY] == 1 ? 3 : 1;
+		} else {
+			score = freq[JOLLY] == 3 ? 5 : freq[JOLLY] == 2 ? 3 : freq[JOLLY] == 1 ? 1 : 0;
+		}
+	}
+
+	bool operator<(const hand_t &other) const
+	{
+		if (score != other.score) {
+			return score < other.score;
+		} else {
+			return strcmp(cards, other.cards) < 0;
+		}
+	}
+};
+
+int main ()
+{
+	vector<hand_t> hands;
+	string cards;
+	uint64_t bid;
+
+	while (cin >> cards >> bid) {
+		hands.emplace_back(cards, bid);
+	}
+
+	sort(hands.begin(), hands.end());
+
+	uint64_t tot = 0;
+	for (size_t i = 0; i < hands.size(); i++) {
+		tot += hands[i].bid * (i + 1);
+	}
+
+	cout << tot << endl;
+}
