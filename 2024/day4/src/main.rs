31c6cca940b8cc95d628bc808b101c5cac929b94
MarcoMeinardi <marcomeinardi01@gmail.com>
2024-12-07 17:56:23 +0100

    Improve day4

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -11,49 +11,28 @@ static XMAS: &[u8] = b"XMAS";
 static DY: [i32; 8] = [-1, -1, -1, 0, 0, 1, 1, 1];
 static DX: [i32; 8] = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-fn check_full_xmas(input: &[Vec<u8>], x: i32, y: i32, dy: i32, dx: i32) -> bool {
-    let last_y = y + dy * 3;
-    let last_x = x + dx * 3;
-
-    if last_y < 0 || last_y >= input.len() as i32 || last_x < 0 || last_x >= input[0].len() as i32 {
-        return false;
-    }
-
-    if input[last_y as usize][last_x as usize] != XMAS[3] {
-        return false;
-    }
-
-    if input[(y + dy * 2) as usize][(x + dx * 2) as usize] != XMAS[2] {
-        return false;
-    }
-
-    return true;
-}
-
-fn check_xmas(input: &[Vec<u8>], x: usize, y: usize) -> i32 {
+fn check_xmas(input: &[Vec<u8>], x: i32, y: i32) -> i32 {
     return DY.iter().zip(DX.iter()).filter(|(&dy, &dx)| {
-        let ny = y as i32 + dy;
-        let nx = x as i32 + dx;
+        let last_y = y + dy * (XMAS.len() - 1) as i32;
+        let last_x = x + dx * (XMAS.len() - 1) as i32;
 
-        if ny < 0 || ny >= input.len() as i32 || nx < 0 || nx >= input[0].len() as i32 {
+        if last_y < 0 || last_y >= input.len() as i32 || last_x < 0 || last_x >= input[0].len() as i32 {
             return false;
         }
 
-        if input[ny as usize][nx as usize] == XMAS[1] {
-            return check_full_xmas(input, x as i32, y as i32, dy, dx);
-        }
-
-        return false;
+        return XMAS.iter().enumerate().skip(1).all(|(i, &c)| {
+            input[(y + dy * i as i32) as usize][(x + dx * i as i32) as usize] == c
+        });
     }).count() as i32;
 }
 
 fn part1(input: &[Vec<u8>]) -> i32 {
-    return (0..input.len()).map(|y|
-        (0..input[0].len())
+    return (0..input.len())
+        .map(|y| (0..input[0].len())
             .filter(|&x| input[y][x] == XMAS[0])
-            .map(|x| check_xmas(input, x, y))
+            .map(|x| check_xmas(input, x as i32, y as i32))
             .sum::<i32>()
-    ).sum();
+        ).sum();
 }
 
 static DY2: [i32; 4] = [-1, -1, 1, 1];
@@ -61,21 +40,20 @@ static DX2: [i32; 4] = [-1, 1, -1, 1];
 
 static MAS: &[u8; 3] = b"MAS";
 
-fn check_x_mas(input: &[Vec<u8>], x: usize, y: usize) -> bool {
-    return DY2.iter().zip(DX2.iter()).filter(|(&dy, &dx)|
-        input[(y as i32 + dy) as usize][(x as i32 + dx) as usize] == MAS[0]
-    ).filter(|(&dy, &dx)|
-        input[(y as i32 - dy) as usize][(x as i32 - dx) as usize] == MAS[2]
-    ).count() == 2;
+fn check_x_mas(input: &[Vec<u8>], x: i32, y: i32) -> bool {
+    return DY2.iter().zip(DX2.iter())
+        .filter(|(&dy, &dx)|
+            input[(y + dy) as usize][(x + dx) as usize] == MAS[0] &&
+            input[(y - dy) as usize][(x - dx) as usize] == MAS[2]
+        ).count() == 2;
 }
 
 fn part2(input: &[Vec<u8>]) -> i32 {
-    return (1..input.len() - 1).map(|y|
-        (1..input[0].len() - 1)
-            .filter(|&x| input[y][x] == MAS[1])
-            .filter(|&x| check_x_mas(input, x, y))
+    return (1..input.len() - 1)
+        .map(|y| (1..input[0].len() - 1)
+            .filter(|&x| input[y][x] == MAS[1] && check_x_mas(input, x as i32, y as i32))
             .count() as i32
-    ).sum();
+        ).sum();
 }
 
 fn main() {
